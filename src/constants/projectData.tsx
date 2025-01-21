0d72f9bb07e9eba413176d4f0d87e49d5b2c0e9a
pksingh1508 <rajiit1212@gmail.com>
2025-01-21 22:20:48 +0530

    new project data

--- src/constants/projectData.tsx
+++ src/constants/projectData.tsx
@@ -1,50 +1,87 @@
 export const projectData = [
-    {  
-        id: 1,
-        title: "Bazar List - Organize your Shopping List",
-        description: "A shopping list app that allows users to create and share shopping lists with others.",
-        description1: "Users can add items to the list, mark them as purchased, and delete them later.",
-        description2: "Having a very clean and simple UI for with better experience.",
-        technologies: ["Expo,", "Expo SQLite,", "React Native,", "TypeScript,", "React Navigation,"],
-        github: "https://github.com/pksingh1508/bazarList_NJH",
-    },
-    {
-        id: 2,
-        title: "BookABarber - Your Personal Barber",
-        description: "A mobile app that allows users to book appointments with barbers.",
-        description1: "Users can view the barbers' profiles, check their availability, and book appointments.",
-        description2: "Barbers can manage their schedules, view upcoming appointments, and mark them as completed.",
-        technologies: ["React Native,", "Supabase,", "Google Maps API,", "TypeScript,", "Expo Router,", "Expo Location,"],
-        github: "https://github.com/pksingh1508/bookabarber",
-    },
-    {
-        id: 3,
-        title: "LearniFi - An EdTech Platform",
-        description: "Build an edTech Platform just like Udemy for student and teacher to collaborate.",
-        description1: "Teacher have functionality to create course and launch it also have admin panels.",
-        description2: "Student can enroll in course and learn from it and Give an honest review that help for other students.",
-        technologies: ["React,", "JavaScript,", "Node.js,", "MongoDB,", "Express,", "Redux,", "Tailwind CSS"],
-        github: "https://github.com/pksingh1508/Learnifi",
-        url: "https://learnifi-seven.vercel.app",
-    },
-    {
-        id: 4,
-        title: "Hackathonwallah - Launch the Hackathons",
-        description: "Build a Modern website for hosting hackathons to all over the world.",
-        description1: "Online hackathons with some great prizes.",
-        description2: "Anyone can participate, build, submit and win great prizes.",
-        technologies: ["Next js,", "TypeScript,", "Clerk,", "Zustand,", "Tailwind CSS"],
-        github: "https://github.com/pksingh1508/hackathonHub",
-        url: "https://hackathonwallah.com",
-    },
-    {
-        id: 5,
-        title: "Bhagavad Gita - Read MahaBharat",
-        description: "Build a Mobile App to learn the Mahabharat.",
-        description1: "It list all the chapters on the Home Page.",
-        description2: "It includes all the verses of the individual chapters.",
-        technologies: ["React-Native,", "TypeScript,", "Rapid API,",],
-        github: "https://github.com/pksingh1508/BhagavadGita_App",
-        url: "https://expo.dev/accounts/pawankumar1212/projects/BhagwadGita/builds/747afc01-bdd6-44b5-adfb-ea9cdf979a97",
-    }
-]
+  {
+    id: 1,
+    title: "Expense App - Organize and Monitor your Expenses",
+    description:
+      "App that allows users to keep track of their expenses and manage their budgets.",
+    description1:
+      "Users can add, edit, and delete transactions, view their transaction history, and see their total balance.",
+    description2: "The app also provides to add wallet and manage the wallet.",
+    technologies: [
+      "Expo,",
+      "Supabase,",
+      "React Native,",
+      "TypeScript,",
+      "Expo Router,",
+      "Zustand"
+    ],
+    github: "https://github.com/pksingh1508/expenses_2.0"
+  },
+  {
+    id: 2,
+    title: "BookABarber - Your Personal Barber",
+    description:
+      "A mobile app that allows users to book appointments with barbers.",
+    description1:
+      "Users can view the barbers' profiles, check their availability, and book appointments.",
+    description2:
+      "Barbers can manage their schedules, view upcoming appointments, and mark them as completed.",
+    technologies: [
+      "React Native,",
+      "Supabase,",
+      "Google Maps API,",
+      "TypeScript,",
+      "Expo Router,",
+      "Expo Location"
+    ],
+    github: "https://github.com/pksingh1508/bookabarber"
+  },
+  {
+    id: 3,
+    title: "LearniFi - An EdTech Platform",
+    description:
+      "Build an edTech Platform just like Udemy for student and teacher to collaborate.",
+    description1:
+      "Teacher have functionality to create course and launch it also have admin panels.",
+    description2:
+      "Student can enroll in course and learn from it and Give an honest review that help for other students.",
+    technologies: [
+      "React,",
+      "JavaScript,",
+      "Node.js,",
+      "MongoDB,",
+      "Express,",
+      "Redux,",
+      "Tailwind CSS"
+    ],
+    github: "https://github.com/pksingh1508/Learnifi",
+    url: "https://learnifi-seven.vercel.app"
+  },
+  {
+    id: 4,
+    title: "Hackathonwallah - Launch the Hackathons",
+    description:
+      "Build a Modern website for hosting hackathons to all over the world.",
+    description1: "Online hackathons with some great prizes.",
+    description2: "Anyone can participate, build, submit and win great prizes.",
+    technologies: [
+      "Next js,",
+      "TypeScript,",
+      "Clerk,",
+      "Zustand,",
+      "Tailwind CSS"
+    ],
+    github: "https://github.com/pksingh1508/hackathonHub",
+    url: "https://hackathonwallah.com"
+  },
+  {
+    id: 5,
+    title: "Bhagavad Gita - Read MahaBharat",
+    description: "Build a Mobile App to learn the Mahabharat.",
+    description1: "It list all the chapters on the Home Page.",
+    description2: "It includes all the verses of the individual chapters.",
+    technologies: ["React-Native,", "TypeScript,", "Rapid API"],
+    github: "https://github.com/pksingh1508/BhagavadGita_App",
+    url: "https://expo.dev/accounts/pawankumar1212/projects/BhagwadGita/builds/5f3c2a4d-ba3a-445f-bf0d-28a4320fd0fc"
+  }
+];
